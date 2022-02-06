f64b40ea355f51cfe1a374b717b340a2d534e160
Noah Cohn <noahc42@gmail.com>
2022-02-06 14:52:41 -0800

    Update m001.cpp

--- tests/m001.cpp
+++ tests/m001.cpp
@@ -296,33 +296,74 @@ glClearColor(0.0f,1.0f,0.0f,1.0f);
 emscripten_set_main_loop((void(*)())renderFrame,0,0);
 }
 
-
 extern "C" {
 void str(){
 strt();
 }}
 
 static inline const char *emscripten_event_type_to_string(int eventType){
-const char *events[]={ "(invalid)","(none)","keypress","keydown","keyup","click","mousedown","mouseup","dblclick","mousemove","wheel","resize","scroll","blur","focus","focusin","focusout","deviceorientation","devicemotion","orientationchange","fullscreenchange","pointerlockchange","visibilitychange","touchstart","touchend","touchmove","touchcancel","gamepadconnected","gamepaddisconnected","beforeunload","batterychargingchange","batterylevelchange","webglcontextlost","webglcontextrestored","(invalid)" };
+const char *events[]={"(invalid)","(none)","keypress","keydown","keyup","click","mousedown","mouseup","dblclick","mousemove","wheel","resize","scroll","blur","focus","focusin","focusout","deviceorientation","devicemotion","orientationchange","fullscreenchange","pointerlockchange","visibilitychange","touchstart","touchend","touchmove","touchcancel","gamepadconnected","gamepaddisconnected","beforeunload","batterychargingchange","batterylevelchange","webglcontextlost","webglcontextrestored","(invalid)"};
 ++eventType;
-if (eventType < 0) eventType=0;
-if (eventType >= sizeof(events)/sizeof(events[0])) eventType=sizeof(events)/sizeof(events[0])-1;
+if(eventType<0)eventType=0;
+if(eventType>=sizeof(events)/sizeof(events[0]))eventType=sizeof(events)/sizeof(events[0])-1;
 return events[eventType];
 }
 
 const char *emscripten_result_to_string(EMSCRIPTEN_RESULT result){
-if (result == EMSCRIPTEN_RESULT_SUCCESS) return "EMSCRIPTEN_RESULT_SUCCESS";
-if (result == EMSCRIPTEN_RESULT_DEFERRED) return "EMSCRIPTEN_RESULT_DEFERRED";
-if (result == EMSCRIPTEN_RESULT_NOT_SUPPORTED) return "EMSCRIPTEN_RESULT_NOT_SUPPORTED";
-if (result == EMSCRIPTEN_RESULT_FAILED_NOT_DEFERRED) return "EMSCRIPTEN_RESULT_FAILED_NOT_DEFERRED";
-if (result == EMSCRIPTEN_RESULT_INVALID_TARGET) return "EMSCRIPTEN_RESULT_INVALID_TARGET";
-if (result == EMSCRIPTEN_RESULT_UNKNOWN_TARGET) return "EMSCRIPTEN_RESULT_UNKNOWN_TARGET";
-if (result == EMSCRIPTEN_RESULT_INVALID_PARAM) return "EMSCRIPTEN_RESULT_INVALID_PARAM";
-if (result == EMSCRIPTEN_RESULT_FAILED) return "EMSCRIPTEN_RESULT_FAILED";
-if (result == EMSCRIPTEN_RESULT_NO_DATA) return "EMSCRIPTEN_RESULT_NO_DATA";
+if(result ==EMSCRIPTEN_RESULT_SUCCESS)return "EMSCRIPTEN_RESULT_SUCCESS";
+if(result==EMSCRIPTEN_RESULT_DEFERRED)return "EMSCRIPTEN_RESULT_DEFERRED";
+if(result==EMSCRIPTEN_RESULT_NOT_SUPPORTED)return "EMSCRIPTEN_RESULT_NOT_SUPPORTED";
+if(result==EMSCRIPTEN_RESULT_FAILED_NOT_DEFERRED)return "EMSCRIPTEN_RESULT_FAILED_NOT_DEFERRED";
+if(result==EMSCRIPTEN_RESULT_INVALID_TARGET)return "EMSCRIPTEN_RESULT_INVALID_TARGET";
+if(result==EMSCRIPTEN_RESULT_UNKNOWN_TARGET)return "EMSCRIPTEN_RESULT_UNKNOWN_TARGET";
+if(result==EMSCRIPTEN_RESULT_INVALID_PARAM)return "EMSCRIPTEN_RESULT_INVALID_PARAM";
+if(result==EMSCRIPTEN_RESULT_FAILED)return "EMSCRIPTEN_RESULT_FAILED";
+if(result==EMSCRIPTEN_RESULT_NO_DATA)return "EMSCRIPTEN_RESULT_NO_DATA";
 return "Unknown EMSCRIPTEN_RESULT!";
 }
 
+int interpret_charcode_for_keyevent(int eventType,const EmscriptenKeyboardEvent *e){
+  
+  // Only KeyPress events carry a charCode. For KeyDown and KeyUp events, these don't seem to be present yet, until later when the KeyDown
+  // is transformed to KeyPress. Sometimes it can be useful to already at KeyDown time to know what the charCode of the resulting
+  // KeyPress will be. The following attempts to do this:
+ 
+if(eventType==EMSCRIPTEN_EVENT_KEYPRESS&&e->which)return e->which;
+if(e->charCode)return e->charCode;
+if(strlen(e->key)==1)return(int)e->key[0];
+if(e->which)return e->which;
+return e->keyCode;
+}
+
+int number_of_characters_in_utf8_string(const char *str){
+if(!str)return 0;
+int num_chars=0;
+while(*str){
+if((*str++&0xC0)!=0x80) ++num_chars; // Skip all continuation bytes
+}
+return num_chars;
+}
+  
+int emscripten_key_event_is_printable_character(const EmscriptenKeyboardEvent *keyEvent){
+return number_of_characters_in_utf8_string(keyEvent->key)==1;
+}
+  
+EM_BOOL key_callback(int eventType, const EmscriptenKeyboardEvent *e, void *userData){
+int dom_pk_code = emscripten_compute_dom_pk_code(e->code);
+printf("%s, key: \"%s\" (printable: %s), code: \"%s\" = %s (%d), location: %lu,%s%s%s%s repeat: %d, locale: \"%s\", char: \"%s\", charCode: %lu (interpreted: %d), keyCode: %s(%lu), which: %lu\n",
+emscripten_event_type_to_string(eventType), e->key, emscripten_key_event_is_printable_character(e) ? "true" : "false", e->code,
+emscripten_dom_pk_code_to_string(dom_pk_code), dom_pk_code, e->location,
+e->ctrlKey ? " CTRL" : "", e->shiftKey ? " SHIFT" : "", e->altKey ? " ALT" : "", e->metaKey ? " META" : "", 
+e->repeat, e->locale, e->charValue, e->charCode, interpret_charcode_for_keyevent(eventType, e), emscripten_dom_vk_to_string(e->keyCode), e->keyCode, e->which);
+if (eventType == EMSCRIPTEN_EVENT_KEYUP) printf("\n"); // Visual cue
+  // Return true for events we want to suppress default web browser handling for.
+  // For testing purposes, want to return false here on most KeyDown messages so that they get transformed to KeyPress messages.
+||(e->keyCode >= DOM_VK_F1 && e->keyCode <= DOM_VK_F24) // Don't F5 refresh the test page to reload.
+||e->ctrlKey // Don't trigger e.g. Ctrl-B to open bookmarks
+||e->altKey // Don't trigger any alt-X based shortcuts either (Alt-F4 is not overrideable though)
+||eventType == EMSCRIPTEN_EVENT_KEYPRESS || eventType == EMSCRIPTEN_EVENT_KEYUP; // Don't perform any default actions on these.
+}
+  
 #define TEST_RESULT(x) if (ret != EMSCRIPTEN_RESULT_SUCCESS) printf("%s returned %s.\n",#x,emscripten_result_to_string(ret));
 int gotClick=0;
 int gotMouseDown=0;
@@ -331,18 +372,17 @@ int gotDblClick=0;
 int gotMouseMove=0;
 int gotWheel=0;
 
-
 EM_BOOL mouse_callback(int eventType,const EmscriptenMouseEvent *e,void *userData){
 printf("%s,screen: (%ld,%ld),client: (%ld,%ld),%s%s%s%s button: %hu,buttons: %hu,movement: (%ld,%ld),target: (%ld,%ld)\n",
 emscripten_event_type_to_string(eventType),e->screenX,e->screenY,e->clientX,e->clientY,
 e->ctrlKey ? " CTRL" : "",e->shiftKey ? " SHIFT" : "",e->altKey ? " ALT" : "",e->metaKey ? " META" : "",
 e->button,e->buttons,e->movementX,e->movementY,e->targetX,e->targetY);
-if (e->screenX != 0 && e->screenY != 0 && e->clientX != 0 && e->clientY != 0 && e->targetX != 0 && e->targetY != 0)  {
-if (eventType == EMSCRIPTEN_EVENT_CLICK) gotClick=1;
-if (eventType == EMSCRIPTEN_EVENT_MOUSEDOWN && e->buttons != 0) gotMouseDown=1;
-if (eventType == EMSCRIPTEN_EVENT_DBLCLICK) gotDblClick=1;
-if (eventType == EMSCRIPTEN_EVENT_MOUSEUP) gotMouseUp=1;
-if (eventType == EMSCRIPTEN_EVENT_MOUSEMOVE && (e->movementX != 0 || e->movementY != 0)) gotMouseMove=1;
+if(e->screenX!=0&&e->screenY!=0&&e->clientX!=0&&e->clientY!=0&&e->targetX!=0&&e->targetY!= 0){
+if(eventType==EMSCRIPTEN_EVENT_CLICK) gotClick=1;
+if(eventType==EMSCRIPTEN_EVENT_MOUSEDOWN&&e->buttons!=0)gotMouseDown=1;
+if(eventType==EMSCRIPTEN_EVENT_DBLCLICK)gotDblClick=1;
+if(eventType==EMSCRIPTEN_EVENT_MOUSEUP)gotMouseUp=1;
+if(eventType==EMSCRIPTEN_EVENT_MOUSEMOVE&&(e->movementX!=0||e->movementY!=0))gotMouseMove=1;
 }
 return 0;
 }
@@ -353,7 +393,7 @@ emscripten_event_type_to_string(eventType),e->mouse.screenX,e->mouse.screenY,e->
 e->mouse.ctrlKey ? " CTRL" : "",e->mouse.shiftKey ? " SHIFT" : "",e->mouse.altKey ? " ALT" : "",e->mouse.metaKey ? " META" : "",
 e->mouse.button,e->mouse.buttons,e->mouse.targetX,e->mouse.targetY,
 (float)e->deltaX,(float)e->deltaY,(float)e->deltaZ,e->deltaMode);
-if (e->deltaY > 0.f || e->deltaY < 0.f){
+if(e->deltaY>0.f||e->deltaY<0.f){
 gotWheel=1;
 }
 return 0;
@@ -376,6 +416,9 @@ ret=emscripten_set_mousemove_callback(EMSCRIPTEN_EVENT_TARGET_WINDOW,0,1,mouse_c
 TEST_RESULT(emscripten_set_mousemove_callback);
 ret=emscripten_set_wheel_callback(EMSCRIPTEN_EVENT_TARGET_WINDOW,0,1,wheel_callback);
 TEST_RESULT(emscripten_set_wheel_callback);
+emscripten_set_keydown_callback(EMSCRIPTEN_EVENT_TARGET_WINDOW,0,1,key_callback);
+emscripten_set_keyup_callback(EMSCRIPTEN_EVENT_TARGET_WINDOW,0,1,key_callback);
+emscripten_set_keypress_callback(EMSCRIPTEN_EVENT_TARGET_WINDOW,0,1,key_callback);
 midd();
 return 1;
 }
