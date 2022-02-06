2282ec32c2c00b22b572a947bc8eedf8a9f821f4
Noah Cohn <noahc42@gmail.com>
2022-02-06 13:29:46 -0800

    Update m001.cpp

--- tests/m001.cpp
+++ tests/m001.cpp
@@ -245,10 +245,10 @@ EM_ASM({console.log("CLICK");});
 
 static void strt(){
 emscripten_cancel_main_loop();
-emscripten_set_mousedown_callback("#canvas",0,1,&userClick);
-emscripten_set_mouseup_callback("#canvas",0,1,&userClick);
-emscripten_set_keydown_callback("#canvas",0,1,&userClick);
-emscripten_set_keyup_callback("#canvas",0,1,&userClick);
+emscripten_set_mousedown_callback("#canvas",0,1,userClick);
+emscripten_set_mouseup_callback("#canvas",0,1,userClick);
+emscripten_set_keydown_callback("#canvas",0,1,userClick);
+emscripten_set_keyup_callback("#canvas",0,1,userClick);
 for(ii=0;ii<2161;ii++){
 vertices[ii]=0.0f;
 }
