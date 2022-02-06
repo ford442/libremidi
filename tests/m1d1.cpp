ad8a996a3abb35730066e6988cc319b60dd9942f
Noah Cohn <noahc42@gmail.com>
2022-02-05 20:50:24 -0800

    Create m1d1.cpp

---
+++ tests/m1d1.cpp
@@ -0,0 +1,56 @@
+#include <libremidi/libremidi.hpp>
+#include <emscripten.h>
+#include <emscripten/html5.h>
+#include <memory>
+#include <EGL/egl.h>
+#include <EGL/eglext.h>
+#include <GLES3/gl3.h>
+#include <GLES3/gl31.h>
+#include <GLES3/gl32.h>
+#define __gl2_h_
+#include <GLES2/gl2ext.h>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <cstdarg>
+#include <cmath>
+#include <cstdio>
+#include <cstdint>
+#include <cstdlib>
+#include <ctime>
+
+int main(int argc,char**){
+std::vector<std::shared_ptr<libremidi::midi_in>>inputs;
+std::vector<std::shared_ptr<libremidi::midi_out>>outputs;
+libremidi::observer::callbacks callbacks{
+.input_added=[&](int idx, const std::string& id){
+std::cout<<"MIDI Input connected: "<<idx<<" - "<<id<<std::endl;
+auto input=std::make_shared<libremidi::midi_in>();
+input->set_callback([](const libremidi::message& msg){
+std::cout<<(int)msg.bytes[0]<<" "
+<<(int)msg.bytes[1]<<" "
+<<(int)msg.bytes[2]<<" "
+<<(double) msg.timestamp<<std::endl;
+});
+input->open_port(idx);
+inputs.push_back(input);
+},
+.input_removed=[&](int idx,const std::string& id){
+},
+.output_added=[&](int idx,const std::string& id){
+std::cout<<"MIDI Output connected: "<<idx<<" - "<<id<<std::endl;
+libremidi::midi_out output{};
+output.open_port(idx);
+output.send_message(std::vector<unsigned char>{0x90,64,100});
+output.send_message(std::vector<unsigned char>{0x80,64,100});
+output.send_message(std::vector<unsigned char>{0x80,64,100});
+output.send_message(std::vector<unsigned char>{0x80,64,100});
+output.send_message(std::vector<unsigned char>{0x80,64,100});
+output.send_message(std::vector<unsigned char>{0x80,64,100});
+},
+.output_removed=[&](int idx,const std::string& id){
+}};
+libremidi::observer obs{libremidi::API::EMSCRIPTEN_WEBMIDI,std::move(callbacks)};
+emscripten_set_main_loop([]{},60,1);
+}
