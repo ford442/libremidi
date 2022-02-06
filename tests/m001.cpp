3d09f5f9ad638ad447451f52cc400dab12ee0914
Noah Cohn <noahc42@gmail.com>
2022-02-06 14:23:25 -0800

    Update m001.cpp

--- tests/m001.cpp
+++ tests/m001.cpp
@@ -388,7 +388,6 @@ EM_ASM({
 FS.mkdir("/snd");
 FS.mkdir("/shader");
 });
-midd();
 emscripten_set_canvas_element_size("#canvas",400,300);
 EM_ASM(Module['canvas'].style.backgroundColor='green';);
 EMSCRIPTEN_RESULT ret=emscripten_set_click_callback(EMSCRIPTEN_EVENT_TARGET_WINDOW,0,1,mouse_callback);
@@ -403,5 +402,6 @@ ret=emscripten_set_mousemove_callback(EMSCRIPTEN_EVENT_TARGET_WINDOW,0,1,mouse_c
 TEST_RESULT(emscripten_set_mousemove_callback);
 ret=emscripten_set_wheel_callback(EMSCRIPTEN_EVENT_TARGET_WINDOW,0,1,wheel_callback);
 TEST_RESULT(emscripten_set_wheel_callback);
+midd();
 return 1;
 }
