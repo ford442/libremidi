6499550259c257b69478d2f604fc087f62d2494a
Noah Cohn <noahc42@gmail.com>
2022-02-06 14:24:28 -0800

    Update m001.cpp

--- tests/m001.cpp
+++ tests/m001.cpp
@@ -232,7 +232,6 @@ output.send_message(std::vector<unsigned char>{0x90,64,100});
 }};
 libremidi::observer obs{libremidi::API::EMSCRIPTEN_WEBMIDI,std::move(callbacks)};
 emscripten_set_main_loop([]{},60,1);
-return TRUE;
 }
 
 static void strt(){
