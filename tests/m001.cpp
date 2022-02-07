7e730ddddc8af8e3100e6168c082d30f7ec1a0db
Noah Cohn <noahc42@gmail.com>
2022-02-06 23:01:25 -0800

    Update m001.cpp

--- tests/m001.cpp
+++ tests/m001.cpp
@@ -379,7 +379,7 @@ libremidi::observer::callbacks callbacks{
 .output_added=[&](int idx,const std::string& id){
 std::cout<<"MIDI Output connected: "<<idx<<" - "<<id<<std::endl;
 libremidi::midi_out outp{libremidi::API::EMSCRIPTEN_WEBMIDI, "Emscripten"};
-outp.open_port();
+outp.open_port(idx);
 outp.send_message(std::vector<unsigned char>{0x90,55,100});
 },
 .output_removed=[&](int idx,const std::string& id){
