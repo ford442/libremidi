0f8b0d9a3f7c2ecfe28dc6869cc8f252cf7da762
Noah Cohn <noahc42@gmail.com>
2022-02-06 18:58:54 -0800

    Update m001.cpp

--- tests/m001.cpp
+++ tests/m001.cpp
@@ -312,14 +312,14 @@ EM_BOOL key_callback(int eventType,const EmscriptenKeyboardEvent *e,void *userDa
 // outp.send_message(std::vector<unsigned char>{0x80, 64, 100});
 int dom_pk_code=emscripten_compute_dom_pk_code(e->code);
 libremidi::midi_out outp;
-outp.open_port(0);
+outp.open_port(idx);
 if(e->keyCode==112){
 EM_ASM({console.log("F1");});
 outp.send_message(std::vector<unsigned char>{0x90,64,100});
 }
 if(e->keyCode==123){
 EM_ASM({console.log("F12");});
-outp.send_message(std::vector<unsigned char>{0x80, 64, 100});
+// outp.send_message(std::vector<unsigned char>{0x80, 64, 100});
 }
 printf("%s, key: \"%s\" (printable: %s), code: \"%s\" = %s (%d), location: %lu,%s%s%s%s repeat: %d, locale: \"%s\", char: \"%s\", charCode: %lu (interpreted: %d), keyCode: %s(%lu), which: %lu\n",
 emscripten_event_type_to_string(eventType),e->key,emscripten_key_event_is_printable_character(e) ? "true" : "false", e->code,
