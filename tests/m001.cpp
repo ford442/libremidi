a01759c9a02ca58407691647eee6dd89ffc34306
Noah Cohn <noahc42@gmail.com>
2022-02-07 03:53:30 -0800

    Update m001.cpp

--- tests/m001.cpp
+++ tests/m001.cpp
@@ -276,7 +276,7 @@ glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,6*sizeof(float),(void*)0);
 glEnableVertexAttribArray(0);
 glVertexAttribPointer(1,3,GL_FLOAT,GL_FALSE,6*sizeof(float),(void*)(3*sizeof(float)));
 glEnableVertexAttribArray(1);
-attrib_position=glGetAttribLocation(shader_program,"location");
+GLuint attrib_position=glGetAttribLocation(shader_program,"location");
 glEnableVertexAttribArray(attrib_position);
 t1=steady_clock::now();
 viewportSizeX=w;
