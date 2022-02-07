de9c691b15f12b4bd8bdcad77f76ac5ea00667a9
Noah Cohn <noahc42@gmail.com>
2022-02-06 19:29:41 -0800

    Update m001.cpp

--- tests/m001.cpp
+++ tests/m001.cpp
@@ -309,26 +309,25 @@ return number_of_characters_in_utf8_string(keyEvent->key)==1;
 }
 
 EM_BOOL key_callback(int eventType,const EmscriptenKeyboardEvent *e,void *userData){
+  
 libremidi::observer::callbacks callbacks{
 .input_added=[&](int idx, const std::string& id){},
 .input_removed=[&](int idx,const std::string& id){},
 .output_added=[&](int idx,const std::string& id){
 std::cout<<"MIDI Output connected: "<<idx<<" - "<<id<<std::endl;
 libremidi::midi_out outp;
-outp.open_port(idx);
+// outp.open_port(idx);
 // output.send_message(std::vector<unsigned char>{0x90,64,100});
-outp.send_message(libremidi::message::note_on(1, 55, 127));
-outp.send_message(libremidi::message::note_off(1, 55, 127));
 },
 .output_removed=[&](int idx,const std::string& id){
 }};
 libremidi::observer obs{libremidi::API::EMSCRIPTEN_WEBMIDI,std::move(callbacks)};int dom_pk_code=emscripten_compute_dom_pk_code(e->code);
 // libremidi::midi_out outp;
-// outp.open_port(idx);
+outp.open_port(0);
+unsigned char bytes[3] = { 144, 110, 40 };
 if(e->keyCode==112){
 EM_ASM({console.log("F1");});
-outp.send_message(std::vector<unsigned char>{0x90,64,100});
-}
+outp.send_message(bytes,sizeof(bytes));
 if(e->keyCode==123){
 EM_ASM({console.log("F12");});
 // outp.send_message(std::vector<unsigned char>{0x80, 64, 100});
