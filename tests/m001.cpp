4a8c8d5b1c59bd83c1676f1754b82c69f5b1a191
Noah Cohn <noahc42@gmail.com>
2022-02-07 15:37:14 -0800

    Update m001.cpp

--- tests/m001.cpp
+++ tests/m001.cpp
@@ -122,7 +122,7 @@ void noteOnGL(int note){
 int aa;
 for(aa=0;aa<note;aa++){
 vertices[(note*aa)+3]=vertices[3]+0.2f;
-vertices[(note*aa)+4]=vertices[100]+&white;
+vertices[(note*aa)+4]=vertices[100]+static_cast<float>(&white);
 vertices[(note*aa)+5]=vertices[3]+0.2f;
 }
 vertices[(note*aa)+6]=white;
