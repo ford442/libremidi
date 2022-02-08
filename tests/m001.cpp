f3dd511309b96f45f02d9655ca7dedec917724cd
Noah Cohn <noahc42@gmail.com>
2022-02-07 16:05:12 -0800

    Update m001.cpp

--- tests/m001.cpp
+++ tests/m001.cpp
@@ -277,10 +277,11 @@ glGenBuffers(1,&VBO);
 glBindVertexArray(VAO);
 glBindBuffer(GL_ARRAY_BUFFER,VBO);
 glBufferData(GL_ARRAY_BUFFER,sizeof(vertices),vertices,GL_STATIC_DRAW);
-glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,6*sizeof(float),(void*)0);
-glEnableVertexAttribArray(0);
+
 glVertexAttribPointer(1,3,GL_FLOAT,GL_FALSE,6*sizeof(float),(void*)(3*sizeof(float)));
 glEnableVertexAttribArray(1);
+  glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,6*sizeof(float),(void*)0);
+glEnableVertexAttribArray(0);
 t1=steady_clock::now();
 viewportSizeX=w;
 viewportSizeY=h;
