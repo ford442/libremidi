bf184b8db55853753e0e5d64120eb8f67ed17162
Noah Cohn <noahc42@gmail.com>
2022-02-07 20:58:48 -0800

    Update emscripten.hpp

--- include/libremidi/detail/emscripten.hpp
+++ include/libremidi/detail/emscripten.hpp
@@ -198,7 +198,7 @@ stop_observing();
 }
 
 void start_observing(){
-EM_ASM({let id=setInterval(Module._libremidi_devices_poll,10000);globalThis.__libreMidi_timer=id;});
+EM_ASM({let id=setTimeout(Module._libremidi_devices_poll,750);globalThis.__libreMidi_timer=id;});
 }
 
 void stop_observing(){
