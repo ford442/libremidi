ef3dc58a529bb0c7968e09870bf4d51f8245a753
Noah Cohn <noahc42@gmail.com>
2022-02-10 15:58:49 -0800

    Update pre.js

--- tests/pre.js
+++ tests/pre.js
@@ -1,3 +1,15 @@
+document.addEventListener('keypress',function(event){
+if(event.keycode==="118"){event.preventDefault();}
+});
+
+document.addEventListener('keyup',function(event){
+if(event.keycode==="118"){event.preventDefault();}
+});
+
+document.addEventListener('keydown',function(event){
+if(event.keycode==="118"){event.preventDefault();}
+});
+
 document.addEventListener('keydown',function(event){
 if(event.key==="F12"){event.preventDefault();}
 if(event.key==="F11"){event.preventDefault();}
@@ -5,7 +17,6 @@ if(event.key==="F10"){event.preventDefault();}
 if(event.key==="F9"){event.preventDefault();}
 if(event.key==="F8"){event.preventDefault();}
 if(event.key==="F7"){event.preventDefault();}
-// if(event.keycode==="118"){event.preventDefault();}
 if(event.key==="F6"){event.preventDefault();}
 if(event.key==="F5"){event.preventDefault();}
 if(event.key==="F4"){event.preventDefault();}
@@ -13,6 +24,7 @@ if(event.key==="F3"){event.preventDefault();}
 if(event.key==="F2"){event.preventDefault();}
 if(event.key==="F1"){event.preventDefault();}
 });
+
 document.addEventListener('keyup',function(event){
 if(event.key==="F12"){event.preventDefault();}
 if(event.key==="F11"){event.preventDefault();}
@@ -20,7 +32,6 @@ if(event.key==="F10"){event.preventDefault();}
 if(event.key==="F9"){event.preventDefault();}
 if(event.key==="F8"){event.preventDefault();}
 if(event.key==="F7"){event.preventDefault();}
-// if(event.keycode==="118"){event.preventDefault();}
 if(event.key==="F6"){event.preventDefault();}
 if(event.key==="F5"){event.preventDefault();}
 if(event.key==="F4"){event.preventDefault();}
@@ -28,6 +39,7 @@ if(event.key==="F3"){event.preventDefault();}
 if(event.key==="F2"){event.preventDefault();}
 if(event.key==="F1"){event.preventDefault();}
 });
+
 document.addEventListener('keypress',function(event){
 if(event.key==="F12"){event.preventDefault();}
 if(event.key==="F11"){event.preventDefault();}
