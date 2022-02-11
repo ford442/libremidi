372dd5b4d312e535f70ff5f4c24363728be7dec5
Noah Cohn <noahc42@gmail.com>
2022-02-10 16:11:56 -0800

    Update pre.js

--- tests/pre.js
+++ tests/pre.js
@@ -1,21 +1,25 @@
 document.addEventListener('keypress',function(event){
 if(event.keycode==="118"){event.preventDefault();
-event.stopPropagation? event.stopPropagation() : event.cancelBubble=true;}
-});
+event.stopPropagation? event.stopPropagation() : event.cancelBubble=true;
+}});
 
 document.addEventListener('keyup',function(event){
-if(event.keycode==="118"){event.preventDefault();}
-});
+if(event.keycode==="118"){event.preventDefault();
+event.stopPropagation? event.stopPropagation() : event.cancelBubble=true;
+}});
 
 document.addEventListener('keydown',function(event){
-if(event.keycode==="118"){event.preventDefault();}
-});
+if(event.keycode==="118"){event.preventDefault();
+event.stopPropagation? event.stopPropagation() : event.cancelBubble=true;
+}});
 
 document.addEventListener('keydown',function(event){
 if(event.key==="F12"){event.preventDefault();}
 if(event.key==="F11"){event.preventDefault();}
 if(event.key==="F10"){event.preventDefault();}
-if(event.key==="F9"){event.preventDefault();}
+if(event.key==="F9"){event.preventDefault();
+event.stopPropagation? event.stopPropagation() : event.cancelBubble=true;
+}
 if(event.key==="F8"){event.preventDefault();}
 if(event.key==="F7"){event.preventDefault();}
 if(event.key==="F6"){event.preventDefault();}
@@ -32,7 +36,9 @@ if(event.key==="F11"){event.preventDefault();}
 if(event.key==="F10"){event.preventDefault();}
 if(event.key==="F9"){event.preventDefault();}
 if(event.key==="F8"){event.preventDefault();}
-if(event.key==="F7"){event.preventDefault();}
+if(event.key==="F7"){event.preventDefault();
+event.stopPropagation? event.stopPropagation() : event.cancelBubble=true;
+}
 if(event.key==="F6"){event.preventDefault();}
 if(event.key==="F5"){event.preventDefault();}
 if(event.key==="F4"){event.preventDefault();}
@@ -47,7 +53,9 @@ if(event.key==="F11"){event.preventDefault();}
 if(event.key==="F10"){event.preventDefault();}
 if(event.key==="F9"){event.preventDefault();}
 if(event.key==="F8"){event.preventDefault();}
-if(event.key==="F7"){event.preventDefault();}
+if(event.key==="F7"){event.preventDefault();
+event.stopPropagation? event.stopPropagation() : event.cancelBubble=true;
+}
 if(event.key==="F6"){event.preventDefault();}
 if(event.key==="F5"){event.preventDefault();}
 if(event.key==="F4"){event.preventDefault();}
