e5b849803f92f36e2513de129b8c3be46f698de6
Noah Cohn <noahc42@gmail.com>
2022-02-10 16:40:49 -0800

    Update pre.js

--- tests/pre.js
+++ tests/pre.js
@@ -1,5 +1,6 @@
 document.addEventListener('keypress',function(event){
-if(event.keycode==="118"){event.preventDefault();
+if(event.keycode==="118"){
+  // event.preventDefault();
 event.stopPropagation? event.stopPropagation() : event.cancelBubble=true;
 }});
 
@@ -9,43 +10,10 @@ event.stopPropagation? event.stopPropagation() : event.cancelBubble=true;
 }});
 
 document.addEventListener('keydown',function(event){
-if(event.keycode==="118"){event.preventDefault();
+if(event.keycode==="118"){
+  // event.preventDefault();
 event.stopPropagation? event.stopPropagation() : event.cancelBubble=true;
 }});
-
-document.addEventListener('keydown',function(event){
-if(event.key==="F12"){event.preventDefault();}
-if(event.key==="F11"){event.preventDefault();}
-if(event.key==="F10"){event.preventDefault();}
-if(event.key==="F9"){event.preventDefault();
-event.stopPropagation? event.stopPropagation() : event.cancelBubble=true;
-}
-if(event.key==="F8"){event.preventDefault();}
-if(event.key==="F7"){event.preventDefault();}
-if(event.key==="F6"){event.preventDefault();}
-if(event.key==="F5"){event.preventDefault();}
-if(event.key==="F4"){event.preventDefault();}
-if(event.key==="F3"){event.preventDefault();}
-if(event.key==="F2"){event.preventDefault();}
-if(event.key==="F1"){event.preventDefault();}
-});
-
-document.addEventListener('keyup',function(event){
-if(event.key==="F12"){event.preventDefault();}
-if(event.key==="F11"){event.preventDefault();}
-if(event.key==="F10"){event.preventDefault();}
-if(event.key==="F9"){event.preventDefault();}
-if(event.key==="F8"){event.preventDefault();}
-if(event.key==="F7"){event.preventDefault();
-event.stopPropagation? event.stopPropagation() : event.cancelBubble=true;
-}
-if(event.key==="F6"){event.preventDefault();}
-if(event.key==="F5"){event.preventDefault();}
-if(event.key==="F4"){event.preventDefault();}
-if(event.key==="F3"){event.preventDefault();}
-if(event.key==="F2"){event.preventDefault();}
-if(event.key==="F1"){event.preventDefault();}
-});
 
 document.addEventListener('keypress',function(event){
 if(event.key==="F12"){event.preventDefault();}
@@ -53,7 +21,8 @@ if(event.key==="F11"){event.preventDefault();}
 if(event.key==="F10"){event.preventDefault();}
 if(event.key==="F9"){event.preventDefault();}
 if(event.key==="F8"){event.preventDefault();}
-if(event.key==="F7"){event.preventDefault();
+if(event.key==="F7"){
+  // event.preventDefault();
 event.stopPropagation? event.stopPropagation() : event.cancelBubble=true;
 }
 if(event.key==="F6"){event.preventDefault();}
@@ -63,3 +32,42 @@ if(event.key==="F3"){event.preventDefault();}
 if(event.key==="F2"){event.preventDefault();}
 if(event.key==="F1"){event.preventDefault();}
 });
+
+document.addEventListener('keydown',function(event){
+if(event.key==="F12"){event.preventDefault();}
+if(event.key==="F11"){event.preventDefault();}
+if(event.key==="F10"){event.preventDefault();}
+if(event.key==="F9"){event.preventDefault();
+}
+if(event.key==="F8"){event.preventDefault();}
+if(event.key==="F7"){
+// event.preventDefault();
+event.stopPropagation? event.stopPropagation() : event.cancelBubble=true;
+}
+if(event.key==="F6"){event.preventDefault();}
+if(event.key==="F5"){event.preventDefault();}
+if(event.key==="F4"){event.preventDefault();}
+if(event.key==="F3"){event.preventDefault();}
+if(event.key==="F2"){event.preventDefault();}
+if(event.key==="F1"){event.preventDefault();}
+});
+
+document.addEventListener('keyup',function(event){
+if(event.key==="F12"){event.preventDefault();}
+if(event.key==="F11"){event.preventDefault();}
+if(event.key==="F10"){event.preventDefault();}
+if(event.key==="F9"){event.preventDefault();}
+if(event.key==="F8"){event.preventDefault();}
+if(event.key==="F7"){
+  // event.preventDefault();
+event.stopPropagation? event.stopPropagation() : event.cancelBubble=true;
+}
+if(event.key==="F6"){event.preventDefault();}
+if(event.key==="F5"){event.preventDefault();}
+if(event.key==="F4"){event.preventDefault();}
+if(event.key==="F3"){event.preventDefault();}
+if(event.key==="F2"){event.preventDefault();}
+if(event.key==="F1"){event.preventDefault();}
+});
+
+
