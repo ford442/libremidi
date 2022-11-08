ccaf05e8caf12d30ed09035d87744bdc36dfb60f
Noah Cohn <noahc42@gmail.com>
2022-11-08 08:26:55 -0800

    Update m001.cpp

--- tests/m001.cpp
+++ tests/m001.cpp
@@ -487,10 +487,79 @@ if(e->keyCode==81){kkey=kkey-377;k=83;midd(m1,k,2);}
 return e->keyCode==DOM_VK_F2||e->keyCode==DOM_VK_F3||e->keyCode==DOM_VK_F4||e->keyCode==DOM_VK_F5||e->keyCode==DOM_VK_F6||e->keyCode==DOM_VK_F7||e->keyCode==DOM_VK_F8||e->keyCode==DOM_VK_F9||e->keyCode==DOM_VK_F10||e->keyCode==DOM_VK_F11||e->keyCode==DOM_VK_F12||e->keyCode==DOM_VK_F1||e->keyCode==DOM_VK_BACK_SPACE||(e->keyCode>=DOM_VK_F1&&e->keyCode<=DOM_VK_F24)||e->ctrlKey||e->altKey||eventType==EMSCRIPTEN_EVENT_KEYPRESS||eventType||eventType==EMSCRIPTEN_EVENT_KEYUP;
 }
 
+}
+
+#include <SDL2/SDL.h>
+SDL_AudioDeviceID dev;
+struct{Uint8* snd;int pos;Uint32 slen;SDL_AudioSpec spec;}wave;
+
+void cls_aud(){
+if(dev!=0){
+SDL_PauseAudioDevice(dev,SDL_TRUE);
+SDL_CloseAudioDevice(dev);
+dev=0;
+return;
+}}
+
+void qu(int rc){
+SDL_Quit();
+return;
+}
+
+void opn_aud(){
+dev=SDL_OpenAudioDevice(NULL,SDL_FALSE,&wave.spec,NULL,0);
+if(!dev){
+SDL_FreeWAV(wave.snd);
+}
+SDL_PauseAudioDevice(dev,SDL_FALSE);
+return;
+}
+
+void SDLCALL bfr(void *unused,Uint8* stm,int len){
+Uint8* wptr;
+int lft;
+wptr=wave.snd+wave.pos;
+lft=wave.slen-wave.pos;
+while (lft<=len){
+SDL_memcpy(stm,wptr,lft);
+stm+=lft;
+len-=lft;
+wptr=wave.snd;
+lft=wave.slen;
+wave.pos=0;
+}
+SDL_memcpy(stm,wptr,len);
+wave.pos+=len;
+return;
+}
+
+void plt(){
+char flnm[24];
+SDL_FreeWAV(wave.snd);
+SDL_SetMainReady();
+if (SDL_Init(SDL_INIT_AUDIO)<0){
+qu(1);
+}
+SDL_strlcpy(flnm,"/snd/sample.wav",sizeof(flnm));
+if(SDL_LoadWAV(flnm,&wave.spec,&wave.snd,&wave.slen)==NULL){
+qu(1);
+}
+wave.pos=0;
+wave.spec.callback=bfr;
+opn_aud();
+return;
+}
+
+extern "C" {
+
 void str(){
 strt();
 }
-
+  
+void pl(){
+plt();
+}
+  
 }
 
 std::vector<std::shared_ptr<libremidi::midi_out>>outputs;
